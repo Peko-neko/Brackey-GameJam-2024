ea0c836cc451645ee4b513d86c90dab93440bf09
Peko-neko <66202899+Peko-neko@users.noreply.github.com>
2024-09-10 13:41:36 +0700

    Added the camera tracking and sprite flip for movements
    Making camera lock-on to player and add some smoothness to it when the player's motion stopped. Also the sprite can now be flipped when moving in opposite direction

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 2f, -5f); //Distance of camera position to player 
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 rotationEulerAngles = new Vector3(20f, 0f, 0f); // Rotation for the camera
+
+    void Start()
+    {
+        // Set the camera rotation using Euler angles
+        transform.rotation = Quaternion.Euler(rotationEulerAngles);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
